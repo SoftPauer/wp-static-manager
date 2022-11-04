a050b22f2cccf816914e3dd258cf0fa2851b1819
andriusm <andriusm@softpauer.com>
2022-11-04 06:06:33 +0000

    not ignoring mobile html folder

--- static-manager.php
+++ static-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name: Static Manager
  * Plugin URI: 
  * Description: Provides a way to update mobile app
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Andrius Murauskas
  * Author URI: 
  * GitHub Plugin URI: https://github.com/SoftPauer/wp-static-manager
